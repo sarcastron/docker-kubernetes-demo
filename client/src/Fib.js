200e69affd22490fafc0079eadf007221779960e
Adam Plante <toomanyadams@gmail.com>
2018-11-13 19:42:49 -0800

    Update the servers

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -5,7 +5,7 @@ class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: '',
+    index: ''
   };
 
   componentDidMount() {
@@ -20,14 +20,16 @@ class Fib extends Component {
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-    this.setState({ seenIndexes: seenIndexes.data });
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
 
     await axios.post('/api/values', {
-      index: this.state.index,
+      index: this.state.index
     });
     this.setState({ index: '' });
   };
@@ -44,15 +46,17 @@ class Fib extends Component {
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
+
+    return entries;
   }
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>Enter your index: </label>
+          <label>Enter your index:</label>
           <input
             value={this.state.index}
             onChange={event => this.setState({ index: event.target.value })}
--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const keys = require('./keys');
+
+// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const redis = require('redis');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Postgres client setup
+// Postgres Client Setup
 const { Pool } = require('pg');
 const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
   password: keys.pgPassword,
-  port: keys.pgPort,
+  port: keys.pgPort
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
@@ -23,25 +24,28 @@ pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
 
-// Redis client setup
+// Redis Client Setup
+const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000,
+  retry_strategy: () => 1000
 });
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
+
 app.get('/', (req, res) => {
-  res.send('Hi there!');
+  res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * FROM values');
+  const values = await pgClient.query('SELECT * from values');
+
   res.send(values.rows);
 });
 
-app.get('/values/current',async (req, res) => {
+app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
@@ -49,20 +53,18 @@ app.get('/values/current',async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
-  if (parseInt(index, 10) > 40) {
+
+  if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('values', index, 'Nothing Yet!');
+  redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-  return res.send({ working: true });
+  res.send({ working: true });
 });
 
-app.listen(5000, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening on 5000');
+app.listen(5000, err => {
+  console.log('Listening');
 });
